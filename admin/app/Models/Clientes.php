f9e6c69d11d8d5b520839f11112eacb73cfcc71a
silviooosilva <gasparsilvio7@gmail.com>
2023-01-07 23:44:40 +0100

    Clientes Model

---
+++ admin/app/Models/Clientes.php
@@ -0,0 +1,18 @@
+<?php
+
+require __DIR__ . "/../../../_app/models/Model.php";
+class clientesModel extends Model
+{
+    public function __construct()
+    {
+        parent::__construct("users", ['nome', 'email', 'celular', 'senha']);
+    }
+
+    /**
+     * @return integer
+     */
+    public function numOfUsers(): int
+    {
+        return $this->find()->count();
+    }
+}
