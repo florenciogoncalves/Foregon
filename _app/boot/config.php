e39b439811cad0f64a46c5480827b7a7acc5bc0a
silviooosilva <gasparsilvio7@gmail.com>
2023-01-18 00:01:33 +0100

    Consts for send mail

--- _app/boot/config.php
+++ _app/boot/config.php
@@ -18,3 +18,18 @@ define("CONF_PASSWD_MIN_LEN", 8);
 define("CONF_PASSWD_MAX_LEN", 40);
 define("CONF_PASSWD_ALGO", PASSWORD_DEFAULT);
 define("CONF_PASSWD_OPTION", ["cost" => 10]);
+
+/**
+ * MAIL
+ */
+define("CONF_MAIL_HOST", "");
+define("CONF_MAIL_PORT", "");
+define("CONF_MAIL_USER", "");
+define("CONF_MAIL_PASS", "");
+define("CONF_MAIL_SENDER", ["name" => "Equipe Foregon", "address" => ""]);
+define("CONF_MAIL_SUPPORT", "");
+define("CONF_MAIL_OPTION_LANG", "br");
+define("CONF_MAIL_OPTION_HTML", true);
+define("CONF_MAIL_OPTION_AUTH", true);
+define("CONF_MAIL_OPTION_SECURE", "tls");
+define("CONF_MAIL_OPTION_CHARSET", "utf-8");
