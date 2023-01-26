8df406358f593eaf65f065f1a5c697a80e424a5b
silviooosilva <gasparsilvio7@gmail.com>
2023-01-26 22:48:43 +0100

    landing-page

--- _app/boot/helpers.php
+++ _app/boot/helpers.php
@@ -2,12 +2,19 @@
 
 include_once __DIR__ . "/config.php";
 
+/**
+ * @param string $email
+ * @return boolean
+ */
 function is_email(string $email): bool
 {
     return filter_var($email, FILTER_VALIDATE_EMAIL);
 }
 
-
+/**
+ * @param string $password
+ * @return boolean
+ */
 function is_password(string $password): bool
 {
     if ($password < CONF_PASSWD_MIN_LEN && $password > CONF_PASSWD_MAX_LEN) {
@@ -15,7 +22,10 @@ function is_password(string $password): bool
     }
     return true;
 }
-
+/**
+ * @param [type] $input
+ * @return void
+ */
 function filter($input)
 {
     return filter_var($input, FILTER_DEFAULT);
@@ -25,7 +35,10 @@ function filter($input)
  * UPLOAD HELPERS
  */
 
-
+/**
+ * @param [type] $file
+ * @return boolean
+ */
 function max_size_allowed($file): bool
 {
     /**
@@ -38,12 +51,20 @@ function max_size_allowed($file): bool
     }
     return true;
 }
-
+/**
+ * @param [type] $file
+ * @param [type] $name
+ * @param integer $seconds
+ * @return string
+ */
 function rename_image($file, $name, int $seconds = 60): string
 {
     return $name . " - " . date('Ymd') . time() + $seconds . mb_strstr($file, '.');
 }
-
+/**
+ * @param string $name
+ * @return boolean
+ */
 function createFolder(string $name = 'produtos'): bool
 {
     if (is_file(__DIR__ . "/../../_storage/{$name}") || is_dir(__DIR__ . "/../../_storage/{$name}")) {
@@ -53,7 +74,11 @@ function createFolder(string $name = 'produtos'): bool
         return true;
     }
 }
-
+/**
+ * @param [type] $origin
+ * @param [type] $destination
+ * @return boolean
+ */
 function uploader($origin, $destination): bool
 {
 
@@ -63,20 +88,29 @@ function uploader($origin, $destination): bool
     return false;
 }
 
-
+/**
+ * @return integer
+ */
 function generateVerificationCode(): int
 {
     return random_int(10000, 99999);
 }
 
-
+/**
+ * @return void
+ */
 function dispatch()
 {
     session_destroy();
     header("Location: ../login.php");
     die;
 }
-
+/**
+ * @param string $string
+ * @param integer $limit
+ * @param string $pointer
+ * @return string
+ */
 function str_limit_words(string $string, int $limit, string $pointer = "..."): string
 {
     $string = trim(filter_var($string, FILTER_SANITIZE_SPECIAL_CHARS));
@@ -91,41 +125,65 @@ function str_limit_words(string $string, int $limit, string $pointer = "..."): s
     return "{$words}{$pointer}";
 }
 
+/**
+ * @param string $value
+ * @return string
+ */
 function hashPassword(string $value): string
 {
     return password_hash($value, PASSWORD_DEFAULT);
 }
 
+/**
+ * @param string $password
+ * @param [type] $hashUser
+ * @return boolean
+ */
 function verifyHash(string $password, $hashUser): bool
 {
     return password_verify($password, $hashUser);
 }
 
-
-function generateToken()
-{
-    return sha1(uniqid(mt_rand(), true));
-}
+/**
+ * @param [type] $number
+ * @return void
+ */
 function convertToBRL($number)
 {
     return number_format($number, 2, ',', '.');
 }
 
+/**
+ * @param string $file
+ * @return void
+ */
 function remove(string $file)
 {
     unlink(__DIR__ . "/../../_storage/produtos/{$file}");
 }
 
+/**
+ * @param string $string
+ * @return string
+ */
 function formatDate(string $string): string
 {
     return mb_strcut($string, 0, 10);
 }
 
+/**
+ * @param string $string
+ * @return string
+ */
 function formatHours(string $string): string
 {
     return mb_strcut($string, 10, 6);
 }
 
+/**
+ * @param string $time
+ * @return string
+ */
 function getHour(string $time): string
 {
 
